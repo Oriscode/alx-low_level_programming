e7647b512fea57124178be892fe3e611d94ae947
oriscode <hello@oriscode.com>
2022-06-26 05:29:02 -0700

    Task4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,2 @@
-/**
- * C program to show the use of puts
- *
- * retun 0;
- *\
-#include <stdio.h>
-int main()
-{
-	puts("Programming is like building a multilingual puzzle");
-
-	return 0;
-}
+#!/bin/bash
+puts("Programming is like building a multilingual puzzle")
