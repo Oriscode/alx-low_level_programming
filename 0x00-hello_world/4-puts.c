ee56c408ddff735ba49b3669f4e4561644110c44
oriscode <hello@oriscode.com>
2022-06-29 13:57:07 -0700

    task4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 /**
  * main - use of puts function to print
+ *
+ * return - always 0
  */
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle\n");
+	puts("Programming is like building a multilingual puzzle");
 	return (0);
 }
