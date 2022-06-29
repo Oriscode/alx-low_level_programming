268998060de8b641394087ada2fd5db441cd9c14
oriscode <hello@oriscode.com>
2022-06-29 08:31:29 -0700

    t5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,5 +1,13 @@
-/* Description: using printf with a return */
 #include <stdio.h>
+/**
+ *
+ * main - Entry point 
+ *
+ * Description: 'the program's description'
+ *
+ * Return: Always 0 (Success)
+ *
+ */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
