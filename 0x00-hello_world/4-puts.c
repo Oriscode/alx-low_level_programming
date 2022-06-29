e6e90e9ffded20b74ee4a39eb90c657ce819cd43
oriscode <hello@oriscode.com>
2022-06-29 14:35:47 -0700

    4t

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,10 +4,10 @@
  *
  * Description: program to print string with puts function
  *
- * return: always 0 (Success)
+ * Return: always 0 (Success)
  */
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle\n");
 	return (0);
 }
