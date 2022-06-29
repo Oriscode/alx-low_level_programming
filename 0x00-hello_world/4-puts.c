5e45181d5223495163fa4d4b23a64b8fc56a424a
oriscode <hello@oriscode.com>
2022-06-29 14:05:01 -0700

    task4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 /**
- * main - use of puts function to print
+ * main - entry point
  *
- * return - always 0
+ * Description: program to print string with puts function
+ *
+ * return: always 0 (Success)
  */
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle");
+	puts("Programming is like building a multilingual puzzle\n");
 	return (0);
 }
