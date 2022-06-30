26f51f8e7c45f44275c862af0cf3b7a14bbba28f
oriscode <hello@oriscode.com>
2022-06-30 08:47:25 -0700

    task6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,7 +12,7 @@ int main(void)
 	printf("%s%lu%s\n", "Size of an int: ", sizeof(int), " byte(s)");
 	printf("%s%lu%s\n", "Size of a long int: ", sizeof(long), " byte(s)");
 	printf("%s%lu%s\n", "Size of a long long int: ",
-		       	sizeof(long long), " byte(s)");
+			sizeof(long long), " byte(s)");
 	printf("%s%lu%s\n", "Size of a float: ", sizeof(float), " byte(s)");
 	return (0);
 }
