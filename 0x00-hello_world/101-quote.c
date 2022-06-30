7fa26319d149e03373cdd37bfeba2e43ee377f99
oriscode <hello@oriscode.com>
2022-06-30 09:47:40 -0700

    t8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,10 +2,10 @@
 /**
  * main - main block
  *
- * Return: return (0) Success
+ * Return: return (1) Success
  */
 int main(void)
 {
 	printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
-	return (0);
+	return (1);
 }
