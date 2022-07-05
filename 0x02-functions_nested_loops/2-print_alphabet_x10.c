fbcd16d8240aa4dcb52da5a232964ea70031a17e
oriscode <hello@oriscode.com>
2022-07-05 15:43:12 -0700

    t2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_alphabet_x10(void) - print alphabet in ten times
+ */
+void print_alphabet_x10(void)
+{
+	int i;
+	int x;
+
+	for (x = 0; x <= 10; x++)
+	{
+		for (j = 'a'; j <= 'z'; j++)
+		{
+			_putchar();
+		}
+		_putchar('\n');
+	}
+}
