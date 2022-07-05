5cd96b52e691869500585b1b11f82a4252bed708
oriscode <hello@oriscode.com>
2022-07-05 14:59:17 -0700

    task1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * main - entry point
+ *
+ * Description - printing alphabet in lower case
+ *
+ * Return: alway(0)
+ */
+void print_alphabet(void)
+{
+	int i;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
