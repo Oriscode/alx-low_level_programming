776349039875a7b46c7f8aad279293a026ac1609
* <hello@oriscode.com>
2022-07-07 14:23:21 +0100

    task2

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -2,25 +2,28 @@
 
 /**
  * largest_number - returns the largest of 3 numbers
- * @var1: first integer
- * @var2: second integer
- * @var3: third integer
- *
+ * @a: first integer
+ * @b: second integer
+ * @c: third integer
  * Return: largest number
  */
 
 int largest_number(int a, int b, int c)
 {
-	int largest;
+    int largest;
 
-	if (a > b && b > c)
-		largest = a;
-	else if (b > a && b > c)
-		largest = b;
-	else if (c > b)
-		largest = c;
-	else
-		largest = b;
+    if (a > b && b > c)
+    {
+        largest = a;
+    }
+    else if (b > a && a > c)
+    {
+        largest = b;
+    }
+    else
+    {
+        largest = c;
+    }
 
-	return (largest);
+    return (largest);
 }
