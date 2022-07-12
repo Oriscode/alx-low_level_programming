9c058564a412f7bd84a71debbeb3b39c8f38210c
* <hello@oriscode.com>
2022-07-12 17:27:34 +0100

    task1

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,13 +1,12 @@
 #include "main.h"
 /**
  *swap_int - this function swap value a to b
- *@*a - first pointer
- *@*b - second pointer
+ *@*a - first pointer variable
+ *@*b - second pointer variable
  */
 void swap_int(int *a, int *b)
 {
+	int m = *a;
 	*a = *b;
-	*b = *a;
-	*a = 98;
-	*b = 42;
+	*b = m;
 }
