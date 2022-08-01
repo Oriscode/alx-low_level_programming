ebf3aa1d560f816da2d40b7f655cc961199abff7
oriscode <hello@oriscode.com>
2022-08-01 14:12:59 -0700

    Structures and typedef

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -6,10 +6,16 @@
  * @age: integer properties of dog
  * @owner: string properties of dog
  */
-struct dog
+typedef struct dog my_dog
 {
 	char *name;
 	float *age;
 	char *owner;
 }
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+my_dog *new_dog(char *name, float age, char *owner);
+void free_dog(my_dog *d);
+
 #endif
