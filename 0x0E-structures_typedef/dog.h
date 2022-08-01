9ef147b11ac0e1d4103ee59418796064431cb2be
* <hello@oriscode.com>
2022-08-01 22:25:51 +0100

    Structures task 0

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,16 +1,21 @@
 #ifndef DOG_H
 #define DOG_H
+
 /**
- * struct dog - defining a dog structure
- * @name: string properties of dog
- * @age: integer properties of dog
- * @owner: string properties of dog
+ * struct dog - structure definition of a dog
+ * @name: character string
+ * @age: integer
+ * @owner: character string
  */
-struct dog
+typedef struct dog
 {
 	char *name;
-	float *age;
+	float age;
 	char *owner;
-}
+} dog_t;
 
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 #endif
