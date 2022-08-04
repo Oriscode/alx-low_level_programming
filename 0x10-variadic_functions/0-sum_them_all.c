ad1ae25b292a07fe5f4369d2a206ee195d61bb79
oriscode <hello@oriscode.com>
2022-08-04 14:59:52 -0700

    task0

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -5,14 +5,21 @@
  *
  * @n: count argument varible name
  *
- * Return: Sum of all argument
+ * Return: sum of all argument
  */
 int sum_them_all(const unsigned int n, ...)
 {
 	int i;
 	int sum;
 	va_list args;
+
 	if (n == 0)
 		return (0);
 	va_start(args, n);
+
+	sum = 0;
+	for (i = 0; i < n; i++)
+		sum += va_args(args, int);
+	va_end(args);
+	return (sum);
 }
